f496fb9233147a9bf9bcf8a87cba69649224c715
xyphsyic <patelkrish0518@gmail.com>
2023-11-11 17:31:52 -0800

    Connected firebase to backend

---
+++ pingo-backend/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDW-VDyd9Iu8_fIHKoUc6g3EicHtCLBLqU",
+  authDomain: "pingo-35l.firebaseapp.com",
+  projectId: "pingo-35l",
+  storageBucket: "pingo-35l.appspot.com",
+  messagingSenderId: "844035613728",
+  appId: "1:844035613728:web:8f4d8827e014f81c69c845"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
