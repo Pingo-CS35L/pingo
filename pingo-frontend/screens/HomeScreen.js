604e258d38d8fa950f97ffb5c4d94004e664a60f
atirumalai <78452887+mememachine01@users.noreply.github.com>
2023-11-20 11:34:51 -0800

    Rearranged home screen prompts into 3x3 bingo card; Modified logout button design

--- pingo-frontend/screens/HomeScreen.js
+++ pingo-frontend/screens/HomeScreen.js
@@ -1,44 +1,103 @@
 import React, { useState } from 'react';
-import { View, ScrollView, Text, Button } from 'react-native';
+import { View, ScrollView, Text, Button, TouchableOpacity, StyleSheet } from 'react-native';
 import { pickPrompts } from '../prompts';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f0f0f0',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  subHeading: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  bingoCard: {
+    backgroundColor: 'white',
+    borderRadius: 10,
+    padding: 10,
+    marginTop: 20,
+  },
+  bingoRow: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  square: {
+    backgroundColor: 'black',
+    margin: 5,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    borderRadius: 8,
+  },
+  squareText: {
+    color: 'white',
+  },
+  logoutButton: {
+    backgroundColor: 'blue',
+    padding: 15,
+    borderRadius: 8,
+    marginTop: 20,
+  },
+  logoutButtonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
 
 function PingoSquare({ prompt }) {
   return (
-    <View style={{ backgroundColor: 'black', margin: 20, padding: 10, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: 'white' }}>{prompt}</Text>
+    <View style={styles.square}>
+      <Text style={styles.squareText}>{prompt}</Text>
     </View>
   );
 }
 
 function PingoCard({ prompts }) {
-  console.log(prompts)
+  const rows = 3;
+  const cols = 3;
+
+  const chunkArray = (arr, size) => {
+    const chunkedArray = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunkedArray.push(arr.slice(i, i + size));
+    }
+    return chunkedArray;
+  };
+
+  const gridPrompts = chunkArray(prompts, cols);
+
   return (
-    <View style={{ backgroundColor: 'white'}}>
-      <Text style={{ color: 'white' }}>Pingo Card</Text>
-      <PingoSquare prompt={prompts[0]}></PingoSquare>
-      <PingoSquare prompt={prompts[1]}></PingoSquare>
-      <PingoSquare prompt={prompts[2]}></PingoSquare>
-      <PingoSquare prompt={prompts[3]}></PingoSquare>
-      <PingoSquare prompt={prompts[4]}></PingoSquare>
-      <PingoSquare prompt={prompts[5]}></PingoSquare>
-      <PingoSquare prompt={prompts[6]}></PingoSquare>
-      <PingoSquare prompt={prompts[7]}></PingoSquare>
-      <PingoSquare prompt={prompts[8]}></PingoSquare>
+    <View style={styles.bingoCard}>
+      {gridPrompts.map((row, rowIndex) => (
+        <View key={rowIndex} style={styles.bingoRow}>
+          {row.map((prompt, colIndex) => (
+            <PingoSquare key={colIndex} prompt={prompt} />
+          ))}
+        </View>
+      ))}
     </View>
   );
 }
 
 const HomeScreen = ({ navigation }) => {
   const [prompts, setPrompts] = useState(pickPrompts());
+
   return (
-    <ScrollView style={{ padding: 20 }}>
-      <Text>Today's Pingo:</Text>
-      <Text>0 squares completed</Text>
-      <PingoCard prompts={prompts}></PingoCard>
-      <Button
-        title="Logout"
-        onPress={() => navigation.navigate('Login')}
-      />
+    <ScrollView style={styles.container}>
+      <Text style={styles.heading}>Today's Pingo</Text>
+      <Text style={styles.subHeading}>0 squares completed</Text>
+      <PingoCard prompts={prompts} />
+      <TouchableOpacity style={styles.logoutButton} onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.logoutButtonText}>Logout</Text>
+      </TouchableOpacity>
     </ScrollView>
   );
 };
