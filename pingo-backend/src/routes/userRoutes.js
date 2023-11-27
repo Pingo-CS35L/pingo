83c659df634253b0972dfd4abf2f2225d51bd369
rishauv <86102276+rishauv@users.noreply.github.com>
2023-11-26 21:48:01 -0800

    Implemented getUserInfo and getPingoStatus
    Used Firestore methods to implement two methods which require fetching data: getUserInfo and getPingoStatus

--- pingo-backend/src/routes/userRoutes.js
+++ pingo-backend/src/routes/userRoutes.js
@@ -62,17 +62,55 @@ userRouter.post("/signup", async (req, res) => {
 
 userRouter.get("/getUserInfo", async (req, res) => {
     const uid = req.currentUserUID;
-    // Implement firebase data schema to fetch user's information (use the uid above to search through the Realtime Database)
+  
+    const userDocRef = firestore.collection('users').doc(uid);
+
+    get(userRef)
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                const userData = snapshot.val();
+                const userInfo = {
+                    first_name: userData.first_name,
+                    last_name: userData.last_name,
+                    total_images_uploaded: userData.total_images_uploaded,
+                    perfect_games: userData.perfect_games
+                };
+                return res.status(200).json({ success: true, userInfo });
+            } else {
+                return res.status(404).json({ success: false, message: "User not found" });
+            }
+        })
+        .catch((error) => {
+            return res.status(500).json({ success: false, message: "Error fetching data" });
+        });
+
+
 });
 
 userRouter.get("/getUserImages", async (req, res) => {
     const uid = req.currentUserUID;
-    // Implement firebase data schema to fetch user's pingo board images
+    // TODO: Implement firebase data schema to fetch user's pingo board images. 
+    // NOTE: Database will hold url to images, so calling get() on the database will return a url which will need to be processed farther. 
 });
 
 userRouter.get("/getPingoStatus", async (req, res) => {
     const uid = req.currentUserUID;
-    // Implement firebase data schema to get how many tiles user has finished in pingo board
+
+    const userDocRef = firestore.collection('users').doc(uid);
+    
+    get(userRef)
+        .then((snapshot) => {
+            if (snapshot.exists()) {
+                const currentScore = snapshot.val();
+                return res.status(200).json({ success: true, current_score: currentScore });
+            } else {
+                return res.status(404).json({ success: false, message: "User not found" });
+            }
+        })
+        .catch((error) => {
+            return res.status(500).json({ success: false, message: "Error fetching data" });
+        });
+    
 });
 
 export { userRouter };
